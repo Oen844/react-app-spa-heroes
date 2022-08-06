fb171097ac365ca21517ebfba185a74b623e5dea
Pau Egea <papau84@gmail.com>
2022-08-06 19:16:30 +0200

    arguments in url

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import {useParams, Navigate} from 'react-router-dom';
+
+import { getHeroById } from '../helpers';
 
 export const HeroPage = () => {
+
+   const {id} = useParams();
+   
+   const hero = getHeroById(id);
+
+   
+   if(!hero) {
+    return <Navigate to= "/marvel"/>
+   }
+
+   
+  
   return (
-    <h1>HeroPage</h1>
+    <h1>{hero.id}</h1>
   )
 }
