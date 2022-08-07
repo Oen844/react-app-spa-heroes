44dcdf55ccaf48d6fb57a502a5beade2a6855de2
Pau Egea <papau84@gmail.com>
2022-08-07 08:25:50 +0200

    searchComponent

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,79 @@
 import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import queryString from 'query-string';
+
+import { useForm } from '../../hooks/useForm'
+import { HeroeCard } from '../components/HeroeCard'
+
 
 export const SearchPage = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const query = queryString.parse (location.search);
+
+  const {q=''}= query;
+
+
+
+  const { onInputChange, searchText } = useForm({
+    searchText: ''
+  });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+
+
+
+    navigate(`?q=${searchText}`);
+  }
+
   return (
-    <h1>SearchPage</h1>
+
+    <>
+      <h1>Search</h1>
+      <hr />
+
+      <div className="row">
+
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form onSubmit={onSearchSubmit}>
+            <input
+              type="text"
+              placeholder='Search a hero'
+              className='form-control'
+              name='searchText'
+              autoComplete='off'
+              value={searchText}
+              onChange={onInputChange}
+
+            />
+            <button className='btn btn-outline-primary mt-1'>
+              Search
+            </button>
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          <div className='alert alert-primary'>
+            Search a hero
+          </div>
+          
+          <div className='alert alert-danger'>
+            No hero with <b> {q} </b>
+          </div>
+
+        </div>
+
+        {/* <HeroeCard {...hero}/> */}
+
+      </div>
+    </>
   )
 }
