fb1a45be4837d6f08776a329f98d6bd43ae9330b
Pau Egea <papau84@gmail.com>
2022-08-11 18:21:40 +0200

    private Routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,22 +1,27 @@
 import { Route, Routes } from "react-router-dom";
 
 import { LoginPage } from "../auth";
-import {HeroesRoutes} from '../heroes';
+import { HeroesRoutes } from '../heroes';
 // import { Navbar } from "../ui/components"
 import { Navbar } from "../ui/components";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
     return (
         <>
 
-       
+
             <Routes>
-                
-                <Route path="login" element={ <LoginPage />} />
+                <Route path="login" element={<LoginPage />} />
 
-                <Route path="/*" element={ <HeroesRoutes />} />
-                
+                <Route path="/*" element={
+                    <PrivateRoute>
+                        <HeroesRoutes />
+                    </PrivateRoute>
+                }
+                />
 
+                {/* <Route path="/*" element={ <HeroesRoutes />} />                */}
             </Routes>
 
         </>
